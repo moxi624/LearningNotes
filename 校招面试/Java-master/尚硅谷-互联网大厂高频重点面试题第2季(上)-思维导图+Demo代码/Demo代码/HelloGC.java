c0489c801de3e4f589d7f44aaf39d9f21eeb89c6
陌溪_ <xzx19950624@qq.com>
2020-07-03 08:33:13 +0800

    add blog

--- 校招面试/Java-master/尚硅谷-互联网大厂高频重点面试题第2季(上)-思维导图+Demo代码/Demo代码/HelloGC.java
+++
@@ -1,9 +0,0 @@
-public class HelloGC {
-    public static int oneAddone(int x,int y){
-        return x+y;
-    }
-    public static void main(String[] args) throws InterruptedException {
-        int res = oneAddone(1,1);
-        System.out.println(res);
-    }
-}
--- 校招面试/Java-master/尚硅谷-互联网大厂高频重点面试题第2季(上)-思维导图+Demo代码/Demo代码/StrongReferenceDemo.java
+++
@@ -1,9 +0,0 @@
-public class StrongReferenceDemo {
-    public static void main(String[] args){
-        Object obj1 = new Object();//这样定义默认的就是强引用
-        Object obj2 = obj1;//obj2引用赋值
-        obj1 = null;
-        System.gc();
-        System.out.println(obj2);
-    }
-}
