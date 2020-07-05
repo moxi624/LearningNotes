3bd41d5141a5d98dab7e7ab6e21be6a7f7896891
陌溪_ <xzx19950624@qq.com>
2020-07-05 15:45:54 +0800

    add blog

---
+++ JVM/Code/src/main/java/com/atguigu/java/chapter02/ClinitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.java.chapter02;
+
+/**
+ * @author: 陌溪
+ * @create: 2020-07-05-8:56
+ */
+public class ClinitTest {
+    // 任何一个类申明后，都会存在一个构造器
+    private int a = 1;
+    public static void main(String[] args) {
+        int b = 2;
+    }
+}
---
+++ JVM/Code/src/main/java/com/atguigu/java/chapter02/HelloApp.java
@@ -0,0 +1,12 @@
+package com.atguigu.java.chapter02;
+
+/**
+ * @author: 陌溪
+ * @create: 2020-07-05-8:42
+ */
+public class HelloApp {
+    private static int a = 1;
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
